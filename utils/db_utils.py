6a22318e7d3c6746237206da144428959bfc5f5a
Lembalemba <franklembalemba3@gmail.com>
2025-03-09 18:19:42 +0100

    switched the database to postgresql

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -5,16 +5,26 @@ from urllib.parse import quote_plus
 # Load environment variables
 load_dotenv()
 
-# Database configuration
-DB_USER = os.environ.get('DB_USER')
-DB_PASSWORD = os.environ.get('DB_PASSWORD')
-DB_HOST = os.environ.get('DB_HOST')
-DB_NAME = os.environ.get('DB_NAME')
+# Database configuration local configuration
+# DB_USER = os.environ.get('DB_USER')
+# DB_PASSWORD = os.environ.get('DB_PASSWORD')
+# DB_HOST = os.environ.get('DB_HOST')
+# DB_NAME = os.environ.get('DB_NAME')
+
+# password = quote_plus(DB_PASSWORD)
+# DB_URI = f"mysql+pymysql://{DB_USER}:{password}@{DB_HOST}/{DB_NAME}"
+
+
+
+# Database configuration render configuration
+DB_USER = os.environ.get('POSTGRES_USER')
+DB_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+DB_HOST = os.environ.get('POSTGRES_HOST')
+DB_NAME = os.environ.get('POSTGRES_DATABASE')
 
 # Connection string
+DB_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
-password = quote_plus(DB_PASSWORD)
-DB_URI = f"mysql+pymysql://{DB_USER}:{password}@{DB_HOST}/{DB_NAME}"
 # DB_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
 print(f"Connecting to database: {DB_NAME} on {DB_HOST}")
